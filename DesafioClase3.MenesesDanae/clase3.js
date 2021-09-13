357cd7c39456df56caf447ccdbbe2c8a412fb7f6
danaemeneses <danaemeneses5@gmail.com>
2021-09-13 19:24:13 -0300

    agregando desafio 3

---
+++ DesafioClase3.MenesesDanae/clase3.js
@@ -0,0 +1,7 @@
+let nombreIngresado = prompt("Ingresa tu nombre:");
+alert("¡¡Bienvenido" + " " + nombreIngresado + "!!");
+
+for (let i = 0; i < 5; i++) {
+    let numeros = parseInt(prompt("Ingresa un numero"));
+    console.log("El numero elegido es:" + numeros);
+}
