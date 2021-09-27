0ef9e7320921110de9b2d538af23cfee2728ba3b
Danae Meneses <90653264+danaemeneses@users.noreply.github.com>
2021-09-27 02:22:57 -0300

    Update mascota.js

--- 1erEntrega.TF/javascript/mascota.js
+++ 1erEntrega.TF/javascript/mascota.js
@@ -4,10 +4,7 @@ const saludar = () => {
 
 saludar()
 
-
-// Segun el peso, si tiene vacunas y chip identificador, la mascota podrá viajar en cabina de avión o no
-
-class MoldeadorAnimal {
+class Animal {
     constructor (pNombre, pEdad, pPesoKG, pVacunas, pChipIdentificador) {
         this.nombre = pNombre;
         this.edad = pEdad;
@@ -17,7 +14,7 @@ class MoldeadorAnimal {
     };
     puedeViajar(){
         if(this.vacunas==="si" && this.chipIdentificador==="si"){
-            return alert("Su mascota cumple las condiciones para viajar")
+            return alert("Su mascota ya cumple las condiciones para viajar")
         }
         return alert("Su mascota aún no cumple las condiciones obligatorias para viajar en avión. Consultenos!")
     }
@@ -28,25 +25,32 @@ class MoldeadorAnimal {
         return alert("Su mascota deberá viajar en bodega.")
     }
 }
-    
 
-let animal1 = new MoldeadorAnimal("Pancho","6 años", 17,"no","no")
-let animal2 = new MoldeadorAnimal("Minnie","1 año", 3,"si","no")
-let animal3 = new MoldeadorAnimal("Negro","4 años", 9,"si","si")
-let animal4 = new MoldeadorAnimal("Raba","1 año", 18,"si","si")
-let animal5 = new MoldeadorAnimal("Pepino","2 años", 7,"si","si")
+let animal1 = new Animal("Pancho","6 años", 17,"no","no")
+let animal2 = new Animal("Minnie","1 año", 3,"si","no")
+let animal3 = new Animal("Negro","4 años", 9,"si","si")
+let animal4 = new Animal("Raba","1 año", 18,"si","si")
+let animal5 = new Animal("Pepino","2 años", 7,"si","si")
+
+const mascotas = [animal1, animal2, animal3, animal4, animal5]
 
 function bicho(){
     let pNombre = prompt("Ingrese el nombre de su mascota: ");
     let pEdad = parseInt(prompt("Ingrese edad de su mascota: "));
     let pPesoKG = parseInt(prompt("Ingrese el peso de su mascota en KG: "));
-    let pVacunas = prompt("¡Tiene las vacunas al día?(si/no): ");
+    let pVacunas = prompt("¿Tiene las vacunas al día?(si/no): ");
     let pChipIdentificador = prompt("¿Tiene colocado chip identificador?(si/no): ");
-    return new MoldeadorAnimal(pNombre, pEdad, pPesoKG, pVacunas, pChipIdentificador)
+    return new Animal(pNombre, pEdad, pPesoKG, pVacunas, pChipIdentificador)
 }
 
-let animal6 = bicho()
+/*****/
 
-console.log(animal1)
+let animal6 = bicho()
 animal6.viajarEnCabina()
 animal6.puedeViajar()
+
+/*****/
+
+mascotas.push(animal6)
+
+console.log(mascotas)
